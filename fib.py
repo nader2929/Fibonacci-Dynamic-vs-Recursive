fd4bbfb62a8e3f1214b7191d00cb32963bb24b6b
Nader <nadersobhi1@hotmail.com>
2021-11-05 20:45:31 +0100

    Changed the display a little bit

--- fib.py
+++ fib.py
@@ -3,19 +3,19 @@ import time
 
 def calc_n_fib(n):
     fib_numbers = np.array([1,1])
-    print(fib_numbers)
+    # print(fib_numbers)
     for i in range(2, n):
         fib_i = fib_numbers[i-1] + fib_numbers[i-2]
-        print(fib_i)
+        #print(fib_i)
         fib_numbers = np.append(fib_numbers, fib_i)
-    print(f"Fibonacci number at n: {fib_numbers[n-1]}")
+    return fib_numbers[n-1] #result
 
 def calc_n_fib_recursive(n):
     if(n == 1 or n == 2):
         result = 1
     else:
         result = calc_n_fib_recursive(n-1) + calc_n_fib_recursive(n-2)
-        print(result)
+        #print(result)
     return result
 
 mode = ""
@@ -24,13 +24,15 @@ while mode != "q":
     if(mode == "1"):
         n = input("Loop\n\tEnter fibonacci number you want to calc: ")
         start = time.time()
-        calc_n_fib(int(n))
+        result = calc_n_fib(int(n))
+        print(f"\tFibonacci number at n: {result}")
         print(f"\tThat took {time.time() - start}")
     elif(mode == "2"):
         n = input("Recursive\n\tEnter fibonacci number you want to calc: ")
         start = time.time()
         calc_n_fib_recursive(int(n))
-        print(f"\tThat took {time.time() - start}")
+        print(f"\tFibonacci number at n: {result}")
+        print(f"\tThat took {time.time() - start}")        
     elif(mode == "q" or mode == "Q"):
         break
     else:
