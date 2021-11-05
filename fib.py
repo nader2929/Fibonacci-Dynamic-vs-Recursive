ee72ebe78cd297a05cc50bf9b9a821a6a1ace96d
Nader <nadersobhi1@hotmail.com>
2021-11-05 21:01:05 +0100

    Added recursive memoization

--- fib.py
+++ fib.py
@@ -18,11 +18,22 @@ def calc_n_fib_recursive(n):
         #print(result)
     return result
 
+#memo is saved values
+def calc_n_fib_recursive(n, memo):
+    if(memo[n-1] != 0):
+        return memo[n-1]
+    if(n == 1 or n == 2):
+        result = 1
+    else:
+        result = calc_n_fib_recursive(n-1, memo) + calc_n_fib_recursive(n-2, memo)
+        memo[n-1] = result
+    return result
+
 mode = ""
 while mode != "q":
-    mode = input("Modes\n\t1. Loop\n\t2. Recursive\n\tQ\\q. Quit\nEnter mode: ")
+    mode = input("Modes\n\t1. Bottom Up\n\t2. Recursive\n\t3. Recursive Memoization\n\tQ\\q. Quit\nEnter mode: ")
     if(mode == "1"):
-        n = input("Loop\n\tEnter fibonacci number you want to calc: ")
+        n = input("Bottom Up\n\tEnter fibonacci number you want to calc: ")
         start = time.time()
         result = calc_n_fib(int(n))
         print(f"\tFibonacci number at n: {result}")
@@ -32,7 +43,14 @@ while mode != "q":
         start = time.time()
         calc_n_fib_recursive(int(n))
         print(f"\tFibonacci number at n: {result}")
-        print(f"\tThat took {time.time() - start}")        
+        print(f"\tThat took {time.time() - start}")
+    elif(mode == "3"):
+        n = input("Recursive Memo\n\tEnter fibonacci number you want to calc: ")
+        start = time.time()
+        memo = np.array([0 for x in range(0, int(n))])
+        result = calc_n_fib_recursive(int(n), memo)
+        print(f"\tFibonacci number at n: {result}")
+        print(f"\tThat took {time.time() - start}")    
     elif(mode == "q" or mode == "Q"):
         break
     else:
